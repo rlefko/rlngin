c0e5121517ea470a3062a5b57fd5fb725c42ecce
Ryan Lefkowitz <rlefkowitz1800@yahoo.com>
2026-04-15 19:21:57 -0400

    🔧 Add seeGE threshold function for SEE-based pruning

--- tests/test_see.cpp
+++ tests/test_see.cpp
@@ -130,3 +130,122 @@ TEST_CASE("SEE: bishop captures knight defended by pawn", "[see]") {
     // Bishop (330) takes knight (320), pawn takes bishop: 320 - 330 = -10
     CHECK(see(board, m) < 0);
 }
+
+TEST_CASE("seeGE: winning capture at threshold 0", "[see]") {
+    ensureInit();
+    Board board;
+    // White pawn on e4, black pawn on d5 undefended
+    board.setFen("4k3/8/8/3p4/4P3/8/8/4K3 w - - 0 1");
+
+    Move m;
+    m.from = stringToSquare("e4");
+    m.to = stringToSquare("d5");
+    m.promotion = None;
+
+    CHECK(seeGE(board, m, 0) == true);
+}
+
+TEST_CASE("seeGE: losing capture at threshold 0", "[see]") {
+    ensureInit();
+    Board board;
+    // White queen on d1, black pawn on d5 defended by pawn on e6
+    board.setFen("4k3/8/4p3/3p4/8/8/8/3QK3 w - - 0 1");
+
+    Move m;
+    m.from = stringToSquare("d1");
+    m.to = stringToSquare("d5");
+    m.promotion = None;
+
+    CHECK(seeGE(board, m, 0) == false);
+}
+
+TEST_CASE("seeGE: positive threshold rejects small gain", "[see]") {
+    ensureInit();
+    Board board;
+    // White knight on e4, black pawn on d5 undefended. SEE = PieceValue[Pawn] = 82
+    board.setFen("4k3/8/8/3p4/4N3/8/8/4K3 w - - 0 1");
+
+    Move m;
+    m.from = stringToSquare("e4");
+    m.to = stringToSquare("d5");
+    m.promotion = None;
+
+    CHECK(seeGE(board, m, 200) == false);
+    CHECK(seeGE(board, m, 82) == true);
+    CHECK(seeGE(board, m, 83) == false);
+}
+
+TEST_CASE("seeGE: negative threshold accepts losing capture", "[see]") {
+    ensureInit();
+    Board board;
+    // White queen captures pawn defended by pawn. SEE ~ 82 - 1025 = -943
+    board.setFen("4k3/8/4p3/3p4/8/8/8/3QK3 w - - 0 1");
+
+    Move m;
+    m.from = stringToSquare("d1");
+    m.to = stringToSquare("d5");
+    m.promotion = None;
+
+    int seeVal = see(board, m);
+    CHECK(seeGE(board, m, -500) == false);
+    CHECK(seeGE(board, m, seeVal) == true);
+    CHECK(seeGE(board, m, seeVal + 1) == false);
+}
+
+TEST_CASE("seeGE: consistency with see() across positions", "[see]") {
+    ensureInit();
+
+    struct TestPos {
+        const char *fen;
+        const char *from;
+        const char *to;
+        PieceType promo;
+    };
+
+    TestPos positions[] = {
+        // Pawn takes pawn
+        {"4k3/8/8/3p4/4P3/8/8/4K3 w - - 0 1", "e4", "d5", None},
+        // Knight takes defended pawn
+        {"4k3/8/4p3/3p4/4N3/8/8/4K3 w - - 0 1", "e4", "d5", None},
+        // Knight takes undefended queen
+        {"4k3/8/3q4/8/4N3/8/8/4K3 w - - 0 1", "e4", "d6", None},
+        // Rook x-ray
+        {"r3k3/r7/8/8/8/8/R7/R3K3 w - - 0 1", "a2", "a7", None},
+        // En passant
+        {"4k3/8/8/3pP3/8/8/8/4K3 w - d6 0 1", "e5", "d6", None},
+        // Promotion capture
+        {"1r2k3/P7/8/8/8/8/8/4K3 w - - 0 1", "a7", "b8", Queen},
+    };
+
+    for (const auto &pos : positions) {
+        Board board;
+        board.setFen(pos.fen);
+
+        Move m;
+        m.from = stringToSquare(pos.from);
+        m.to = stringToSquare(pos.to);
+        m.promotion = pos.promo;
+
+        int seeVal = see(board, m);
+        CHECK(seeGE(board, m, seeVal) == true);
+        CHECK(seeGE(board, m, seeVal + 1) == false);
+    }
+}
+
+TEST_CASE("seeGE: quiet move to attacked square", "[see]") {
+    ensureInit();
+    Board board;
+    // White knight on e4, black pawn on d5 attacks e4's potential squares.
+    // Knight moves to d6 which is attacked by pawn on e7.
+    board.setFen("4k3/4p3/8/8/4N3/8/8/4K3 w - - 0 1");
+
+    Move m;
+    m.from = stringToSquare("e4");
+    m.to = stringToSquare("d6");
+    m.promotion = None;
+
+    // Moving to a square attacked by a pawn: SEE = -PieceValue[Knight] = -337
+    CHECK(seeGE(board, m, 0) == false);
+    CHECK(seeGE(board, m, -337) == true);
+    CHECK(seeGE(board, m, -336) == false);
+}
