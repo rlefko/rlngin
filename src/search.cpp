39d39f5dd5d2f68ac012a6bb8d0a8a55cb6336f1
Ryan Lefkowitz <rlefkowitz1800@yahoo.com>
2026-04-15 11:48:54 -0400

    ✂️ Add reverse futility pruning to negamax search

--- src/search.cpp
+++ src/search.cpp
@@ -236,6 +236,18 @@ static int negamax(Board &board, int depth, int ply, int alpha, int beta, Search
 
     bool inCheck = isInCheck(board);
 
+    // Static eval for pruning decisions (unreliable when in check)
+    int staticEval = inCheck ? -INF_SCORE : evaluate(board);
+
+    // Reverse futility pruning: if static eval is far above beta at shallow depth,
+    // assume this node will fail high
+    if (!inCheck && depth <= 3 && beta - alpha == 1 && beta > -MATE_SCORE + MAX_PLY) {
+        int rfpMargin = 120 * depth;
+        if (staticEval - rfpMargin >= beta) {
+            return staticEval - rfpMargin;
+        }
+    }
+
     int bestScore = -INF_SCORE;
     Move bestMove = moves[0];
 
