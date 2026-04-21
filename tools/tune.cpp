a210bd2954302d9246726df9d2c84e4c57d8b35d
Ryan Lefkowitz <rlefkowitz1800@yahoo.com>
2026-04-21 17:44:06 -0400

    🎛️ Register the new pawn structure terms with the Texel tuner

--- tools/tune.cpp
+++ tools/tune.cpp
@@ -194,6 +194,12 @@ static std::vector<ParamRef> collectParams() {
     addMgEgConstr("IsolatedPawnPenalty", &evalParams.IsolatedPawnPenalty, nonPositive());
     addMgEgConstr("DoubledPawnPenalty", &evalParams.DoubledPawnPenalty, nonPositive());
     addMgEgConstr("BackwardPawnPenalty", &evalParams.BackwardPawnPenalty, nonPositive());
+    addMgEgConstr("WeakUnopposedPenalty", &evalParams.WeakUnopposedPenalty, nonPositive());
+    addMgEgConstr("DoubledIsolatedPenalty", &evalParams.DoubledIsolatedPenalty, nonPositive());
+    for (int i = 0; i < 2; i++)
+        addMgEgConstr("BlockedPawnPenalty[" + std::to_string(i) + "]",
+                      &evalParams.BlockedPawnPenalty[i], nonPositive());
+    addMgEg("PhalanxBonus", &evalParams.PhalanxBonus);
 
     return out;
 }
@@ -522,6 +528,14 @@ static void printCurrentValues() {
     std::cout << "    " << fmtScore(evalParams.IsolatedPawnPenalty) << ", // IsolatedPawnPenalty\n";
     std::cout << "    " << fmtScore(evalParams.DoubledPawnPenalty) << ", // DoubledPawnPenalty\n";
     std::cout << "    " << fmtScore(evalParams.BackwardPawnPenalty) << ", // BackwardPawnPenalty\n";
+    std::cout << "    " << fmtScore(evalParams.WeakUnopposedPenalty)
+              << ", // WeakUnopposedPenalty\n";
+    std::cout << "    " << fmtScore(evalParams.DoubledIsolatedPenalty)
+              << ", // DoubledIsolatedPenalty\n";
+    std::cout << "    {" << fmtScore(evalParams.BlockedPawnPenalty[0]) << ", "
+              << fmtScore(evalParams.BlockedPawnPenalty[1])
+              << "}, // BlockedPawnPenalty (rel rank 5, 6)\n";
+    std::cout << "    " << fmtScore(evalParams.PhalanxBonus) << ", // PhalanxBonus\n";
     std::cout << "};\n";
 }
 
